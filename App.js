5c3ac53ad09be47e42c79766b7bb43805cee493b
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-06-18 23:24:43 +0500

    First commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppContainer from "./src/navigations/AppNavigation";
+
+export default function App() {
+	return <AppContainer />;
+}
